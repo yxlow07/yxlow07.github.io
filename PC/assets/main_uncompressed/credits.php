0bfda7cebb374ea2d51a3bc8f1318b7045205081
yxlow07 <M-8717828@moe-dl.edu.my>
2023-01-26 15:52:13 +0800

    Initial push

---
+++ PC/assets/main_uncompressed/credits.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Primary Meta Tags -->
+    <title>Credits</title>
+    <meta name="title" content="Credits">
+    <meta name="description" content="Author of Purata Calculator">
+
+    <!-- Open Graph / Facebook / Discord-->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://purata-counter-js.herokuapp.com/">
+    <meta property="og:title" content="Credits">
+    <meta property="og:description" content="Author of Purata Calculator">
+    <meta property="og:image" content="https://purata-counter-js.herokuapp.com/assets/images/home.png">
+    <meta name="theme-color" content="#E0E0E0">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://purata-counter-js.herokuapp.com/">
+    <meta property="twitter:title" content="Credits">
+    <meta property="twitter:description" content="Author of Purata Calculator">
+    <meta property="twitter:image" content="https://purata-counter-js.herokuapp.com/assets/images/home.png">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
+    <title>Credits</title>
+</head>
+<body class="h-100 text-center text-white bg-dark">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <div class="text-black fw-bolder d-flex">
+            <img src="assets/images/favicon.ico" alt="Icon" width="30px" class="d-inline-block align-text-top">
+            Purata Calculator
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-3 fw-bolder">
+                <a class="nav-link" aria-current="page" href="/">Home</a>
+                <a class="nav-link" href="https://github.com/yxlow07/purata-counter-js" target="_blank">Source</a>
+                <a class="nav-link" href="target_calc.php">Target Calculator</a>
+            </div>
+        </div>
+    </div>
+</nav>
+<div class="container p-5" style="font-size: 400%">
+    Content: <a href="https://github.com/yxlow07" target="_blank">yxlow07</a><br>
+    Bug Fixes: <a href="https://github.com/yxlow07" target="_blank">yxlow07</a><br>
+    Contact: <a href="https://github.com/yxlow07" target="_blank">yxlow07</a>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
